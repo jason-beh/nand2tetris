3d8bff07cdd59d79cf9586c797846af6a4029f2a
Jason Beh <behjieshen@gmail.com>
2022-06-26 20:38:09 +0930

    Code module in assembler

---
+++ compilers/assembler/test/main/java/com/jasonbeh/CodeTest.java
@@ -0,0 +1,46 @@
+package main.java.com.jasonbeh;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CodeTest {
+
+    private Code code;
+
+    @Before
+    public void init() {
+        code = new Code();
+    }
+
+    @Test
+    public void getDestThatExist() {
+        assertEquals("001", code.dest("M"));
+    }
+
+    @Test
+    public void getDestThatDoesNotExist() {
+        assertNull(code.dest("I_DON'T_EXIST"));
+    }
+
+    @Test
+    public void getCompThatExist() {
+        assertEquals("0110000", code.comp("A"));
+    }
+
+    @Test
+    public void getCompThatDoesNotExist() {
+        assertNull(code.comp("I_DON'T_EXIST"));
+    }
+
+    @Test
+    public void getJumpThatExist() {
+        assertEquals("100", code.jump("JLT"));
+    }
+
+    @Test
+    public void getJumpThatDoesNotExist() {
+        assertNull(code.jump("I_DON'T_EXIST"));
+    }
+}
